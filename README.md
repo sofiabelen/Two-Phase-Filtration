4075b2c580a5fcc2ab598064336f8726aaa01714
Sofia Lopez <sofiabelenlopez00@gmail.com>
2021-02-03 23:06:32 +0300

    Adding 1 phase filtration solver

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Cavity Flow for Ideal Gas
 ![Alt text](img/ideal-gas.svg?raw=true "1")
+![Alt text](img/porous-medium-darcy.svg?raw=true "1")
--- porous-medium-darcy.jl
+++ porous-medium-darcy.jl
@@ -1,31 +1,53 @@
 ## Physics:
 ##
-## We simulate 2d cavity flow for compressible fluid,
-## more precisely, for an ideal gas at constant temperature.
-## Therefore, from the equation of state, we know that
-## P / ρ = 1 (if we choose the units such that the constants
-## terms are reduced to 1). 
+## We simulate 2d flow of ideal gas through a porous medium
+## with the help of Darcy's law.
 ## 
-## The equations used are the continuity and the momentum equations
+## v⃗ = -μ⁻¹ K̂ ⋅∇P,
 ##
-## The velocity in the x-direction on the top is constant,
-## there are walls on the bottom, left and right edges.
+## where K̂, a second-order tensor in the general case, is
+## the specific permeability. It depends only on the
+## geometry of the mediumm. Here we assume isotropy
+## of space, so K is a scalar. μ is the dynamic viscocity.
 ##
-## →→→→→→→→
-## |      |
-## |      |
-## --------
+## Continuity equation:
+##
+## φ ⋅∂ρ / ∂t + div(ρv⃗) = 0
+##
+## where φ is the porosity.
+##
+## So, the equations used are the continuity equation and
+## Darcy's equation, which replaces the momentum equation,
+## used in our last program.
+##
+## Another change with respect to the ideal gas program
+## is that we will set the boundary conditions using ghost cells.
+## This way, we can make use of the central difference scheme for
+## boundary conditions which consist of partial derivatives,
+## which is second-order.
+##
+## The boundary conditions consist of walls on the top
+## and bottom, and constant pressure on either side.
+##
+## For now, our velocities will be equal to 0 at the beginning.
+##
+## ---------
+## ->     ->
+## ->     ->
+## ---------
 ##
 ## Initial conditions:
-## u = u0 = 0.05 at y = 2
-## v⃗ = (u, v) = 0⃗ everywhere else
-## ρ = p = p0 everywhere (Atmospheric pressure)
+## v⃗ = (u, v) = 0⃗ everywhere
+## P = Pin, Pout at x = 0, 2 (respectively)
+## P = P0 everywhere else
 ##
 ## Boundary conditions:
-## u = 1 at y = 2
-## ∂p / ∂y = 0 at y = 0
-## ρ = p = p0 at y = 2
-## ∂p / ∂x = 0 at x = 0, 2
+## ∂u / ∂x = 0 at x = 0, 2
+## P = Pin at x = 0
+## P = Pout at x = 2
+## u = 0 at y = 0, 2
+## ∂P / ∂y = 0 at y = 0, 2
+## v = 0 at x = 0, 2 and y = 0, 2 (is this okay???)
 
 using PyPlot
 
@@ -40,24 +62,30 @@ nsteps = convert(Int64, duration / Δt)
 nx = convert(Int64, 2.0 / Δx)
 ny = convert(Int64, 2.0 / Δy)
 
-## Viscocity
-ν = 0.1
+## Porosity
+φ = 0.7
 
-## Atmospheric pressure
-p0 = 1.0
+## Specific permeability
+K = 1e-17
 
-## Velocity at the top
-u0 = 0.05
+## Dynamic viscocity (value used: air)
+μ = 0.018
+
+## Pressures at left and right side.
+Pin = 1.5
+Pout = 0.5
+P0 = 1.0
 
 u = zeros(nx, ny)
 v = zeros(nx, ny)
 ρ = zeros(nx, ny)
 
-ρ[:, :] .= p0
-u[:, ny] .= u0
+for i = 2 : nx - 1
+    ρ[i, :] .= Pin + (Pout - Pin) * i / nx
+end
 
 function update!(ρnext, unext, vnext, ρwork, uwork, vwork,
-        Δt, Δx, Δy, u0, ν, nx, ny)
+        Δt, Δx, Δy, nx, ny)
     for j = 2 : ny - 1
         for i = 2 : nx - 1
             ## Auxiliary variables to avoid typos when
@@ -84,76 +112,52 @@ function update!(ρnext, unext, vnext, ρwork, uwork, vwork,
             vs = vwork[i, j - 1]
     
             ## Continuity equation to calculate ρ
-            ## ∂ρ / ∂t + div(ρv⃗) = 0
-            ρnext[i, j] = ρc - Δt * (uc * (ρe - ρw) / (2.0 * Δx) +
+            ## φ ⋅∂ρ / ∂t + div(ρv⃗) = 0
+            ρnext[i, j] = ρc - Δt / φ * (uc * (ρe - ρw) / (2.0 * Δx) +
                                      vc * (ρn - ρs) / (2.0 * Δy) +
                                      ρc * (ue - uw) / (2.0 * Δx) +
                                      ρc * (vn - vs) / (2.0 * Δy))
-
         end
     end
     
     ## Question about view: https://stackoverflow.com/questions/65979923/what-is-happening-behind-the-scenes-in-julias-view-function-a3-viewa
 
-    ## Enforcing boundary conditions for ρ = P
-    ρnext[1, :] = @view ρnext[2, :]
-    ρnext[nx, :] = @view ρnext[nx - 1, :]
-    
-    ρnext[:,  1] = @view ρnext[:, 2]
-    ρnext[:, ny] .= p0
+    ## Boundary conditions using ghost cells
+    ## ρ = ρleft, ρright at x = 0, 2
+    ρnext[1, :] .= 2.0 * Pin .- ρnext[2, :]
+    ρnext[nx, :] .= 2.0 * Pout .- ρ[nx - 1, :]
+    ## ∂ρ / ∂y = 0 at y = 0, 2
+    ρnext[:, ny] .= ρnext[:, ny - 2]
+    ρnext[:, 1] .= ρnext[:, 3]
     
     ## Here we use a central difference scheme to discretize the second
     ## order derivatives, so we iterate over the 'inner' grid
     for j = 2 : ny - 1
         for i = 2 : nx - 1
             ## Auxiliar variables
-            ρc = ρwork[i, j]
             ρe = ρwork[i + 1, j]
             ρw = ρwork[i - 1, j]
             ρn = ρwork[i, j + 1]
             ρs = ρwork[i, j - 1]
     
-            uc = uwork[i, j]
-            ue = uwork[i + 1, j]
-            uw = uwork[i - 1, j]
-            un = uwork[i, j + 1]
-            us = uwork[i, j - 1]
+            ## Darcy's law
+            ## v⃗ = -μ⁻¹ K̂ ⋅∇P
+            unext[i, j] = -K / μ * (ρe - ρw) / (2 * Δx)
     
-            vc = vwork[i, j]
-            ve = vwork[i + 1, j]
-            vw = vwork[i - 1, j]
-            vn = vwork[i, j + 1]
-            vs = vwork[i, j - 1]
-    
-            ## Momentum equation
-            ## ∂v⃗ / ∂t + (v⃗ ⋅∇)v⃗ = - (1 / ρ) ∇P + ν∇²v⃗
-            ## -> we later replace this by Darcy's law
-            viscous_term_u = ν * ((uw - 2.0 * uc + ue) / Δx^2 +
-                                  (un - 2.0 * uc + us) / Δy^2)
-    
-            viscous_term_v = ν * ((vw - 2.0 * vc + ve) / Δx^2 +
-                                  (vn - 2.0 * vc + vs) / Δy^2)
-    
-            pressure_term_u = -(1 / ρc) * (ρe - ρw) / (2.0 * Δx)
-    
-            pressure_term_v = -(1 / ρc) * (ρn - ρs) / (2.0 * Δy)
-    
-            inertia_term_u = uc * (ue - uw) / (2.0 * Δx) +
-                             vc * (un - us) / (2.0 * Δy)
-    
-                             inertia_term_v = uc * (ve - vw) / (2.0 * Δx) +
-                             vc * (vn - vs) / (2.0 * Δy- vw) / (2.0 * Δx)
-    
-            unext[i, j] = uc + Δt * (-inertia_term_u + pressure_term_u +
-                                     viscous_term_u)
-    
-            vnext[i, j] = vc + Δt * (-inertia_term_v + pressure_term_v +
-                                     viscous_term_v)
+            vnext[i, j] = -K / μ * (ρn - ρs) / (2 * Δy)
         end
     end
+
+    ## Boundary conditions on u, again using ghost cells
+    ## ∂u / ∂x = 0 at x = 0, 2
+    unext[1, :] = unext[3, :]
+    unext[nx, :] = unext[nx - 2, :]
+    ## u = 0 at y = 0, 2
+    u[:, ny] = -u[:, ny - 1]
+    u[:, 1] = -u[:, 2]
 end
 
-function cavity_flow!(ρ, u, v, Δx, Δy, Δt, nsteps, nx, ny, u0, ν)
+function filtration!(ρ, u, v, Δx, Δy, Δt, nsteps, nx, ny)
     ## Helper arrays to store values from previous iteration
     ρwork, uwork, vwork = ρ, u, v
     unext = copy(u)
@@ -162,7 +166,7 @@ function cavity_flow!(ρ, u, v, Δx, Δy, Δt, nsteps, nx, ny, u0, ν)
     
     for t = 1 : nsteps
         update!(ρnext, unext, vnext, ρwork, uwork, vwork,
-                Δt, Δx, Δy, u0, ν, nx, ny)
+                Δt, Δx, Δy, nx, ny)
 
         ## Swap next and current and continue iteration
         ρnext, ρwork = ρwork, ρnext
@@ -207,17 +211,17 @@ function plot(u, v, ρ)
     fig.colorbar(pos, ax=ax)
     cp = contour(X, Y, ρ', cmap=matplotlib.cm.viridis)
 
-    PyPlot.title("Cavity Flow for Ideal Gas")
+    PyPlot.title("2d filtration of ideal gas through a porous medium")
 
     # PyPlot.svg(true)
     # savefig("plot.svg")
     
-    savefig("img/ideal-gas.png")
-    savefig("img/ideal-gas.svg")
+    savefig("img/porous-medium-darcy.png")
+    savefig("img/porous-medium-darcy.svg")
 end
 
 ## Run our solver
-ρ, u, v = cavity_flow!(ρ, u, v, Δx, Δy, Δt, nsteps, nx, ny, u0, ν)
+ρ, u, v = filtration!(ρ, u, v, Δx, Δy, Δt, nsteps, nx, ny)
 
 ## Plotting
 plot(u, v, ρ)
