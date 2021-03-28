4787f4a91e6693921ea077c9f1a754393eb523d1
Sofia Lopez <sofiabelenlopez00@gmail.com>
2021-03-28 17:34:43 +0300

    BC on inlet for saturation and BC on velocities using Darcy

--- 2phase-filtration.jl
+++ 2phase-filtration.jl
@@ -136,66 +136,91 @@ function darcy!(; f::Function,
     end
 end
 
-## Question: Is it necessry to write these explicitly
-# function boundaryDensity!(ρnext::AbstractArray{Float64, 2})
-#     nx, ny = size(ρnext)
-#     ## Question
-#     ## ρ = 0 'behind walls'
-#     # @. @views ρnext[1, :] = 0
-#     # @. @views ρnext[nx, :] = 0
-#     # @. @views ρnext[nx ÷ 2 + 1: nx, 1] = 0
-#     ## Or ρ = 0 at walls ??
-#     # @. @views ρnext[1, :] = -ρnext[2, :]
-#     # @. @views ρnext[nx, :] = -ρnext[nx - 1, :]
-#     # @. @views ρnext[nx ÷ 2 + 1: nx, 1] = 
-#     #     -ρnext[nx ÷ 2 + 1: nx, 2]
-#     ## ∂ρ / ∂n = 0
-# 
-#     ## ∂ρ / ∂y = 0 at entrance and exit
-#     @. @views ρnext[:, ny] = ρnext[:, ny - 1]
-#     @. @views ρnext[1: nx ÷ 2, 1] = ρnext[1: nx ÷ 2, 2]
-# end
+function boundaryDensity!(
+        ρ::AbstractArray{T, 3},
+        P::AbstractArray{T, 2}) where T<:AbstractFloat
+    nx, ny = size(ρ)
 
-function boundaryPressure!(Pnext::Array{T, 2},
-        Pin::T, Pout::T) where T<:AbstractFloat
-    nx, ny = size(Pnext)
-    ## P = Pin at y = 0 and x ∈ [0, 1]
-    @. @views Pnext[1: nx ÷ 2, 1] =
-        2 * Pin - Pnext[1: nx ÷ 2, 2]
+    function tait(P)
+        B = 35e6
+        P₀ = 1e5
+        C = 0.2105
+        ρ₀ = 616.18
+        return ρ₀ / (1 - C * log10((B + P) / (B + P₀)))
+    end
 
-    ## P = Pout at y = 2
-    @. @views Pnext[:, ny] = 2 * Pout - Pnext[:, ny - 1]
+    function idealgas(P)
+        coeff = 8.314 * 298 / 0.029
+        return P / coeff
+    end
 
-    ## ∂P / ∂x = 0 at x = 0, 2
-    @. @views Pnext[nx, :] = Pnext[nx - 1, :]
-    @. @views Pnext[1, :] = Pnext[2, :]
+    ## We find the densities from the equation of state, then
+    @. @views ρ[1, :, 1]  = idealgas(P[:, 1])
+    @. @views ρ[1, :, ny] = idealgas(P[:, ny])
+    @. @views ρ[1, 1, :]  = idealgas(P[1, :])
+    @. @views ρ[1, nx, :] = idealgas(P[nx, :])
 
-    ## ∂P / ∂y = 0 at y = 0 x ∈ [1, 2]
-    @. @views Pnext[nx ÷ 2 + 1: nx, 1] =
-        Pnext[nx ÷ 2 + 1: nx, 2]
+    @. @views ρ[2, :, 1]  = tait(P[:, 1])
+    @. @views ρ[2, :, ny] = tait(P[:, ny])
+    @. @views ρ[2, 1, :]  = tait(P[1, :])
+    @. @views ρ[2, nx, :] = tait(P[nx, :])
 end
 
-function boundaryVelocity!(unext::AbstractArray{T, 2},
-        vnext::AbstractArray{T, 2}) where T<:AbstractFloat
-    nx, ny = size(unext)
+function boundarySaturation!(ψ::T, M₁::T, M₂::T,
+        ρ::AbstractArray{T, 3},
+        s::AbstractArray{T, 2},
+        P::AbstractArray{T, 2}) where T<:AbstractFloat
+    nx, ny = size(ρ)
+    ## Inlet
+    ## We derive the saturation from the molar composition and
+    ## the densities.
+    @. @views s[1, 1: nx ÷ 2, 1] = (ρ[1, 1: nx ÷ 2, 1] /
+            ρ[2, 1: nx ÷ 2, 1] * M₁ / M₂ / ψ + 1)^(-1)
+end
+
+function boundaryPressure!(P::Array{T, 2},
+        Pin::T, Pout::T) where T<:AbstractFloat
+    nx, ny = size(P)
+    ## P = Pin at y = 0 and x ∈ [0, 1]
+    @. @views P[1: nx ÷ 2, 1] =
+        2 * Pin - P[1: nx ÷ 2, 2]
+
+    ## P = Pout at y = 2
+    @. @views P[:, ny] = 2 * Pout - P[:, ny - 1]
+
+    ## ∂P / ∂x = 0 at x = 0, 2
+    @. @views P[nx, :] = P[nx - 1, :]
+    @. @views P[1, :] = P[2, :]
+
+    ## ∂P / ∂y = 0 at y = 0 x ∈ [1, 2]
+    @. @views P[nx ÷ 2 + 1: nx, 1] =
+        P[nx ÷ 2 + 1: nx, 2]
+end
+
+function boundaryVelocity!(; K::T, μ::T, f::Function,
+        s::AbstractArray{T, 2},
+        u::AbstractArray{T, 2},
+        v::AbstractArray{T, 2},
+        P::AbstractArray{T, 2}) where T<:AbstractFloat
+    nx, ny = size(u)
     ## u = 0 at x = 0, 2 (walls)
-    @. @views unext[1, :] = -unext[2, :]
-    @. @views unext[nx, :] = -unext[nx - 1, :]
+    @. @views u[1, :] = -u[2, :]
+    @. @views u[nx, :] = -u[nx - 1, :]
 
     ## v = 0 at y = 0 and x ∈ [1, 2]
-    @. @views vnext[1, nx ÷ 2 + 1: nx] =
-        -vnext[2, nx ÷ 2 + 1: nx]
+    @. @views v[1, nx ÷ 2 + 1: nx] =
+        -v[2, nx ÷ 2 + 1: nx]
 
-    ## ∂v / ∂y = 0 at y = 2
-    @views vnext[:, ny] = vnext[:, ny - 1]
-
-    ## ∂v / ∂y = 0 at y = 0 and x ∈ [0, 1]
-    @views vnext[1: nx ÷ 2, 1] =  vnext[1: nx ÷ 2, 2]
+    ## Darcy 
+    @views v[:, ny] = -K / μ * f(s[:, ny]) *
+        (P[:, ny] - P[:, ny - 1]) / Δy
+    @views v[1: nx ÷ 2, 1] =  -K / μ * f(s[1: nx ÷ 2, 1]) *
+        (P[1: nx ÷ 2, 2] - P[1: nx ÷ 2, 1]) / Δy
 end
 
 function update!(; ρnext, unext, vnext, Pnext, snext,
         ρwork, uwork, vwork,
-        Δt, Δx, Δy, Pin, Pout, φ, K, μ)
+        Δt, Δx, Δy, Pin, Pout, φ, K, μ, M, ψ)
     nx, ny = size(ρnext)
     coeff = 8.314 * 298 / 0.029
 
@@ -204,34 +229,36 @@ function update!(; ρnext, unext, vnext, Pnext, snext,
             ρnext=ρnext[k, :, :], ρwork=ρwork[k, :, :],
             uwork=uwork[k, :, :], vwork=vwork[k, :, :])
     end
-    # for k = 1 : 2
-    #     @views boundaryDensity!(ρnext[k, :, :])
-    # end
+
     for index in CartesianIndices(Pnext)
         i, j = Tuple(index)
         Pnext[i, j], snext[i, j] =
             findPressure(ρ̂₁=ρnext[1, i, j],
                          ρ̂₂=ρnext[2, i, j], V=Δx * Δy)
     end
+
     boundaryPressure!(Pnext, Pin, Pout)
+    boundaryDensity!(ρnext, Pnext) 
+    boundarySaturation!(ψ, M[1], M[2], ρnext, snext, Pnext) 
     
     f1(s) = s^2
     f2(s) = (1 - s)^2
     f = [f1, f2]
+
     for k = 1 : 2
-        @views darcy!(f=f[k],  unext=unext[k, :, :],
+        @views darcy!(f=f[k], unext=unext[k, :, :],
                vnext=vnext[k, :, :], Pnext=Pnext,
                s=(k == 1 ? snext : snext.-1), K=K,
-               μ=μ, Δx=Δx, Δy=Δy)
-    end
-    for k = 1 : 2
-        @views boundaryVelocity!(unext[k, :, :],
-                                 vnext[k, :, :])
+               μ=μ[k], Δx=Δx, Δy=Δy)
+
+        @views boundaryVelocity!(f=f[k], K=K, μ=μ[k],
+            s=(k == 1 ? snext : snext.-1),
+            u=unext[k, :, :], v=vnext[k, :, :], P=Pnext)
     end
 end
 
 function filtration!(; Δx, Δy, Δt, nsteps, K, φ, μ,
-        Pin, Pout, sys)
+        Pin, Pout, sys, M, ψ)
     syswork = sys
     sysnext = copy(sys)
     
@@ -252,7 +279,9 @@ function filtration!(; Δx, Δy, Δt, nsteps, K, φ, μ,
                       Pout = Pout,
                       φ = φ,
                       K = K,
-                      μ = μ
+                      μ = μ,
+                      M = M,
+                      ψ = ψ
                      )
         update!(; Parameters...)
 
@@ -318,16 +347,23 @@ let
     ## Specific permeability
     K = 1e-12
     
-    ## Dynamic viscocity (for air)
-    μ = 18e-6
+    ## Dynamic viscocity
+    μ = [1.8e-5, 2,14e-4]
     
     ## Pressures at top and bottom, and initial pressure
     Pin = 1e6
     Pout = 1e5
     P0 = Pout
 
-    ## Initial saturation
+    ## Initial Saturation
     s0 = 0.75
+
+    ## Molar masses
+    M = [0.028, 0.07215]
+
+    ## Molar Composition on the Inlet
+    ## ψ = ν₁ / ν₂ = (m₁ / M₁) / (m₂ / M₂)
+    ψ = 0.3
 
     coeff = 8.314 * 298 / 0.029
 
@@ -351,7 +387,9 @@ let
                   μ = μ,
                   Pin = Pin,
                   Pout = Pout,
-                  sys = sys
+                  sys = sys,
+                  M = M,
+                  ψ = ψ
                  )
 
     filtration!(; Parameters...)
